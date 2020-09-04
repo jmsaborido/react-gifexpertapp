137f64518c39de09d0a105c05954b7e2d9a05f27
José María Saborido <josemariasaborido94@gmail.com>
2020-09-04 17:10:36 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { GifExpertApp } from './GifExpertApp';
+
+ReactDOM.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
+
+// Crear component GifExpertAPP
+// <h2>GifExpertApp</h2>
+// <hr/>
