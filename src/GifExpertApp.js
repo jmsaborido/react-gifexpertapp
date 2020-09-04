4d4d7a221e71590fdf73391e1e5180b159b2f475
José María Saborido <josemariasaborido94@gmail.com>
2020-09-04 17:41:38 +0200

    Add(C70):Categories list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,23 @@
 // rafc para crearlo
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => setCategories([...categories, prompt('¿Que categoria quieres añadir?')]);
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map((category) => {
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </>
     );
 }
