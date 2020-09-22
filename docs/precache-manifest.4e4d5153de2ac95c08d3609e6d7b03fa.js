39caecdffc419ce77661d8edd638812f619be0b8
José María Saborido <josemariasaborido94@gmail.com>
2020-09-22 11:34:25 +0200

    Add(C85):Rename build to docs

---
+++ docs/precache-manifest.4e4d5153de2ac95c08d3609e6d7b03fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a41dc641f48e4adca02bcd4863711f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "812d1b2de997a2e37a9f",
+    "url": "/static/css/main.95982de6.chunk.css"
+  },
+  {
+    "revision": "312ab32313dd1e262b6c",
+    "url": "/static/js/2.f4d28284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4d28284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812d1b2de997a2e37a9f",
+    "url": "/static/js/main.615bd73b.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
